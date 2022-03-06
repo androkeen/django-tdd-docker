e03e65fe037d3ddad3d454345b7e16a09c1fdb73
Andro <chvertkini.andro@gmail.com>
2022-03-06 14:29:33 +0400

    Initial commit

---
+++ app/drf_project/urls.py
@@ -0,0 +1,12 @@
+# app/drf_project/urls.py
+
+from django.contrib import admin
+from django.urls import include, path
+
+from .views import ping
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("ping/", ping, name="ping"),
+    path("", include("movies.urls")),
+]
